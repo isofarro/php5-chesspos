e6a0b4d2ce5931c69c5130acf17eb5012cbf793b
Mike Davies <isofarro2@gmail.com>
2014-01-04 11:32:59 +0000

    Convert a board representation into a gif using GD

---
+++ lib/ChessPos/BoardImage.php
@@ -0,0 +1,66 @@
+<?php
+namespace ChessPos;
+
+
+class BoardImage {
+    protected $board;
+    protected $image;
+    protected $pieceDir  = '/home/isofarro/Sites/php5-chesspos/share/pieces/default/32/';
+    protected $pieceSize = 32;
+
+    public function __construct($board) {
+        $this->board = $board;
+    }
+
+    public function outputImage() {
+        $image = $this->generate();
+
+        header('Content-Type: image/gif');
+        imagegif($image);
+    }
+
+    public function saveImage($filePath) {
+        $image = $this->generate();
+        imagegif($image, $filePath);
+    }
+    
+    public function generate() {
+        if (empty($this->image)) {
+            $this->image = $this->_generateImage();
+        }
+
+        return $this->image;
+    }
+
+
+    protected function _generateImage() {
+        $boardImage = \imagecreate(256, 256)
+                    or die("Cannot Instantiate new GD image stream");
+
+        $background = \imagecolorallocate($boardImage, 0, 0, 0);
+        $board = $this->board->getBoard();
+
+        for ($square=0; $square < 64; $square++) {
+            $piece      = $board[$square];
+            $pieceImage = $this->_getPieceImage($piece);
+            $offsetX    = ($square % 8) * $this->pieceSize; 
+            $offsetY    = floor($square / 8) * $this->pieceSize;
+
+            \imagecopy($boardImage, $pieceImage, $offsetX, $offsetY, 0, 0, $this->pieceSize, $this->pieceSize);
+        }
+
+        return $boardImage;
+    }
+
+    protected function _getPieceImage($piece) {
+        if (empty($this->imageCache[$piece])) {
+            $pieceImage = "{$this->pieceDir}{$piece}.gif";
+            $image = \imagecreatefromgif($pieceImage);
+            $this->imageCache[$piece] = $image;
+        }
+
+        return $this->imageCache[$piece];
+    }
+}
+
+?>
